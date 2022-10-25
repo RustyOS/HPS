b55b500039fb8d3490c9614380788cf064bc8574
rusty <rusty@C1>
2022-10-25 17:42:32 +0200

    Facebook Test

--- HPS/HPS_C1.cs
+++ HPS/HPS_C1.cs
@@ -1,36 +1,63 @@
-﻿using NUnit.Framework;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HPS_C1
+using System.Threading;
+using NUnit.Framework;
+namespace HPS
 {
-    class HPS_C1
+    class HPSS
     {
-        IWebDriver driver;
-
+        //create the reference for the browser  
+        IWebDriver driver = new ChromeDriver();
         [SetUp]
-        public void startBrowser()
+        public void Initialize()
         {
-            driver = new ChromeDriver("D:\\3rdparty\\chrome");
-        }
+            //navigate to URL  
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            Thread.Sleep(2000);
+            driver.FindElement(By.Id("u_0_15_v6")).Click();
+            Thread.Sleep(2000);
+            //Maximize the browser window  
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
 
+
+            
+        }
         [Test]
-        public void test()
+        public void ExecuteTest()
         {
-            driver.Url = "http://www.google.co.in";
-        }
+            
 
+            //identify the google search button  
+            IWebElement fb1 = driver.FindElement(By.Id("u_0_15_v6"));
+
+            // click on the Google search button  
+            fb1.Click();
+            Thread.Sleep(3000);
+
+            //identify the username text box  
+            IWebElement ele = driver.FindElement(By.Id("email"));
+            //enter the username value  
+            ele.SendKeys("jairen.maddix@minutestep.com");
+            Thread.Sleep(2000);
+            Console.Write("username value is entered \n");
+            //identify the password text box  
+            IWebElement ele1 = driver.FindElement(By.Name("passContainer"));
+            //enter the password value  
+            ele1.SendKeys("Leptir031");
+            Console.Write("password is entered \n");
+            //click on the Login button  
+            IWebElement ele2 = driver.FindElement(By.Id("u_0_5_cj"));
+            ele2.Click();
+            Thread.Sleep(3000);
+            Console.Write("login button is clicked \n");
+        }
         [TearDown]
-        public void closeBrowser()
+        public void EndTest()
         {
+            //close the browser  
             driver.Close();
         }
-
     }
 }
